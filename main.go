4c50b0e690b571cb18b204ef3ea988dfd0b583e4
liuyang <liuyang06@agree.com.cn>
2026-04-28 18:10:06 +0800

    chore: clean up sample code

--- main.go
+++ main.go
@@ -12,19 +12,17 @@ import (
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
 	app := NewApp()
 
-	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "git-manager",
-		Width:  1024,
-		Height: 768,
+		Title:  "Git Manager",
+		Width:  1280,
+		Height: 800,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		OnStartup:  app.startup,
+		OnShutdown: app.shutdown,
 		Bind: []interface{}{
 			app,
 		},
